a0f385e6fb16595a549b5b45fbc3faf68b3e4d37
janavmakkar <janavmakkar70@gmail.com>
2023-01-31 21:31:16 +0530

    added register end point

---
+++ users/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    #     extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    # def create(self, validated_data):
+    #     user = User.objects.create_user(**validated_data)
+    #     return user
+
+    # def update(self, instance, validated_data):
+    #     password = validated_data.pop('password', None)
+    #     user = super(UserSerializer, self).update(instance, validated_data)
+
+    #     if password is not None:
+    #         user.set_password(password)
+    #         user.save()
--- users/views.py
+++ users/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import exceptions
 from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def register(request):
+    data=request.data
+    
+    # if User.objects.filter(email=data['email']).exists():
+    #     raise exceptions.APIException('User with this email already exists')
+    
+    if data['password'] != data['confirm_password']:
+        raise exceptions.APIException('Passwords do not match')
+    
+    serializer=UserSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+
+    return Response(serializer.data)
 
 @api_view(['GET'])
-def  users(request):
+def users(request):
     users=User.objects.all()
     return Response(users)
