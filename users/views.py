0ba89c1bf6da34c3a0aaaf251546474f470d1d75
janavmakkar <janavmakkar70@gmail.com>
2023-02-02 23:04:27 +0530

    Added register function

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from rest_framework import exceptions
 from .models import User
 from .serializers import UserSerializer
 
+# Register
 @api_view(['POST'])
 def register(request):
     data=request.data
@@ -11,12 +12,27 @@ def register(request):
     if data['password'] != data['confirm_password']:
         raise exceptions.APIException('Passwords do not match')
     
-    serializer=UserSerializer(data=data)
+    serializer=UserSerializer(data= data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
 
     return Response(serializer.data)
 
+# Login
+@api_view(['POST'])
+def login(request):
+    email=request.data.get('email')
+    password=request.data.get('password')
+    user=User.objects.filter(email=email).first()
+    if( user is None):
+        raise exceptions.AuthenticationFailed('User not found')
+    if not user.check_password(password):
+        raise exceptions.AuthenticationFailed('Wrong Password!')
+    return Response('Success')
+
+
+
+# Users
 @api_view(['GET'])
 def users(request):
     serializer=UserSerializer( User.objects.all(),many=True)
