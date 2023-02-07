53f79903def75bbe3a57a1dc83fe1129c779feee
janavmakkar <janavmakkar70@gmail.com>
2023-02-08 05:22:35 +0530

    added roles create api

--- users/serializers.py
+++ users/serializers.py
@@ -21,8 +21,18 @@ class PermissionSerializer(serializers.ModelSerializer):
         model=Permission
         fields='__all__'
 
+class PermissionRelatedSerializer(serializers.StringRelatedField):
+    # function to how to show data from db(show whole object, instead of permission id's)
+    def to_representation(self, value):
+        return PermissionSerializer(value).data
+
+    # function to how to save data in db while creating a new Role
+    # (save permission id's, instead asking for whole permission object)
+    def to_internal_value(self, data):
+        return data
+
 class RoleSerializer(serializers.ModelSerializer):
-    permissions=PermissionSerializer(many=True)
+    permissions=PermissionRelatedSerializer(many=True)
     class Meta:
         model=Role
         fields='__all__'
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import exceptions, viewsets
+from rest_framework import exceptions, viewsets,status
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated 
 from .authentication import generate_access_token, JWTAuthentication
@@ -88,7 +88,12 @@ class RoleViewset(viewsets.ViewSet):
         })
     
     def create(self,request):
-        pass
+        serializer=RoleSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({
+            'data':serializer.data
+        },status=status.HTTP_201_CREATED)
 
     def retrieve(self,request, pk=None):
         pass
